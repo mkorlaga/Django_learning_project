a58a7556b99f50c30f4bc64acc9182a798a285ac
mkorlaga <17402897+mkorlaga@users.noreply.github.com>
2020-08-29 15:49:28 +0200

    Class view product app + Navbar button create

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
-from .views import (product_create_view,
-                    ProductDetailView, # product_detail_view,
-                    product_delete_view,
-                    ProductListView, # product_list_view
+from .views import (ProductCreateView, # product_create_view,
+                    ProductDetailView,  # product_detail_view,
+                    ProductListView,  # product_list_view
+                    ProductDeleteView, ProductCreateView,  # product_delete_view,
                     )
 from django.urls import path
 
@@ -9,7 +9,7 @@ from django.urls import path
 app_name = 'products'
 urlpatterns = [
     path("", ProductListView.as_view(template_name="products/product_list.html"), name='product_list_view'),     # path("", product_list_view, name='product_list_view'),
-    path('create/', product_create_view, name='product_create_view'),
+    path('create/', ProductCreateView.as_view(template_name='products/product_create.html'), name='product_create_view'),       # path('create/', product_create_view, name='product_create_view'),
     path("<int:product_id>/", ProductDetailView.as_view(template_name='products/detail.html'), name='product_detail_view'),     # path("<int:product_id>/", product_detail_view, name='product_detail_view'),
-    path("<int:product_id>/delete/", product_delete_view, name='product_delete_view'),
+    path("<int:product_id>/delete/", ProductDeleteView.as_view(), name='product_delete_view'),    # path("<int:product_id>/delete/", product_delete_view, name='product_delete_view'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,30 +1,48 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, DeleteView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Product
 from .forms import ProductForm
 
 
-def product_create_view(request, *args, **kwargs):
-    initial_data = {
+# def product_create_view(request, *args, **kwargs):
+#     initial_data = {
+#         'price': 199.99,
+#         'description': 'Default description',
+#         'title': 'Default title'
+#     }
+#     # to edit certain object we get the object (hardcoded below) and pass 'obj' as 'instance=obj' parameter to our form
+#     # obj = Product.objects.get(id=1)
+#     # form = ProductForm(instance=obj)
+#     if request.method == 'POST':
+#         form = ProductForm(request.POST)        # passing the request.POST data stright as form without checking if request.method is POST or GET or other
+#         if form.is_valid():
+#             form.save()                                 # ==Product.objects.create(title=request.POST.get('title'),description=request.POST.get('description'), price=request.POST.get('price')) as fields in products/forms.py
+#             form = ProductForm(initial=initial_data)                        # rerendering the form so it is empty
+#     else:
+#         form = ProductForm(initial=initial_data)
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'products/product_create.html', context=context)
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    initial = {
         'price': 199.99,
         'description': 'Default description',
         'title': 'Default title'
     }
-    # to edit certain object we get the object (hardcoded below) and pass 'obj' as 'instance=obj' parameter to our form
-    # obj = Product.objects.get(id=1)
-    # form = ProductForm(instance=obj)
-    if request.method == 'POST':
-        form = ProductForm(request.POST)        # passing the request.POST data stright as form without checking if request.method is POST or GET or other
-        if form.is_valid():
-            form.save()                                 # ==Product.objects.create(title=request.POST.get('title'),description=request.POST.get('description'), price=request.POST.get('price')) as fields in products/forms.py
-            form = ProductForm(initial=initial_data)                        # rerendering the form so it is empty
-    else:
-        form = ProductForm(initial=initial_data)
-    context = {
-        'form': form
-    }
-    return render(request, 'products/product_create.html', context=context)
+
+    # Use fields if there is no 'forms.py' file and no specific forms were created.
+    # fields = [
+    #     'title',
+    #     'description',
+    #     'price'
+    # ]
 
 
 # def product_detail_view(request, *args, **kwargs):
@@ -43,16 +61,21 @@ class ProductDetailView(DetailView):
         return render(request, template_name=self.template_name, context={'object': product})
 
 
-def product_delete_view(request, *args, **kwargs):
-    obj = get_object_or_404(Product, id=kwargs.get('product_id'))
-    if request.method == 'POST':
-        obj.delete()
-        redirect('product_create_view')
-    context = {
-        'object': obj
-    }
-    return render(request, "products/product_delete.html", context=context)
+# def product_delete_view(request, *args, **kwargs):
+#     obj = get_object_or_404(Product, id=kwargs.get('product_id'))
+#     if request.method == 'POST':
+#         obj.delete()
+#         redirect('products:product_create_view')
+#     context = {
+#         'object': obj
+#     }
+#     return render(request, "products/product_confirm_delete.html", context=context)
 
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('products:product_list_view')
+    pk_url_kwarg = 'product_id'                             #  umożliwia użycie <int:product_it>  w url routingu zamiast <int:pk>
 
 # def product_list_view(request, *args, **kwargs):
 #     queryset = Product.objects.all()
@@ -66,7 +89,4 @@ class ProductListView(ListView):
     model = Product
     context_object_name = 'object_list'
     paginate_by = 5
-
-
-#TODO
-# Dokończyć zmianę funkcyjnych na klasy
+    ordering = ['id']
