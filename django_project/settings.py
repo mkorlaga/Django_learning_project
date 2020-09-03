1776bf8c48d6074cd8006ff4560c7c0736e823be
mkorlaga <17402897+mkorlaga@users.noreply.github.com>
2020-09-03 16:49:26 +0200

    Restore email reset credentials

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = os.getenv('EMAIL')                # in case of an error, need to enable gmail external login.
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')   # not so safe way of authorisation
